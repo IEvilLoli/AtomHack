9d3d3ec94d735088f27b428a3ce32f8fa4586469
gorbushin_v_a <gorbushin.volodja@mail.ru>
2023-05-24 15:28:22 +0300

    comments

--- pdf_parser.py
+++ pdf_parser.py
@@ -1,14 +1,23 @@
 from PyPDF2 import PdfFileReader, PdfReader
+import pypdf
+from tabula import read_pdf
 
-pdf_document = "test.pdf"
-with open(pdf_document, "rb") as filehandle:
-    pdf = PdfReader(filehandle)
 
-    info = pdf.metadata
-    pages = len(pdf.pages)
-    print("Количество страниц в документе: %i\n\n" % pages)
-    print("Мета-описание: ", info)
-    for i in range(pages):
-        page = pdf.pages[i]
-        print("Стр.", i, " мета: ", page, "\n\nСодержание;\n")
-        print(page.extract_text())
+def pdf_parse(filepath):
+    pdf_document = filepath
+    with open(pdf_document, "rb") as filehandle:
+        pdf = PdfReader(filehandle)
+
+        info = pdf.metadata
+        pages = len(pdf.pages)
+        print("Количество страниц в документе: %i\n\n" % pages)
+        print("Мета-описание: ", info)
+        for i in range(pages):
+            page = pdf.pages[i]
+            print("Стр.", i, " мета: ", page, "\n\nСодержание;\n")
+            print(page.extract_text())
+
+
+if __name__ == '__main__':
+    pdf_parse("test.pdf")
+    print(0)
